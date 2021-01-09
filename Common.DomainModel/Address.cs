44bca94f89179fd1fc7cc88740cd32899cf39a2d
Marco Fabbian <marco.fabbian@gmail.com>
2021-01-09 21:13:15 +0100

    Added DomainModel RealEstate and DomainServices for RealEstate repo + Unittests (#2)

---
+++ Common.DomainModel/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Common.DomainModel
+{
+    public class Address
+    {
+        public string Road { get; set; }
+        public string Number { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ Real.DomainServices/IRealPropertyRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Real.DomainModel;
+
+namespace Real.DomainServices
+{
+    public interface IRealPropertyRepository
+    {
+        public RealProperty Get(Guid id);
+
+        public IList<RealProperty> GetList();
+
+        public bool Set(RealProperty realProperty);
+    }
+}
