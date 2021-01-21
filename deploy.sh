b0214a49734fcc3384018d5edaae5dacccc351a8
Marco Fabbian <marco.fabbian@gmail.com>
2021-01-21 14:44:09 +0100

    Edit deployment scripts (#18)
    * Added execution riths to the deployment scripts
    
    * Fix typo error
    
    * Fix comand error
    
    * Fix comand error
    
    * Fix comand error

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-source_directory=$WORKSPACE/artifacts
-target_directory=/var/www/real.com/dot_net_api
+source_directory=$WORKSPACE/artifacts/
+target_directory=/var/www/real.com/dot_net_api/
 
-cp -R "$source_directory/" "$target_directory/"
+cp -R $source_directory $target_directory
