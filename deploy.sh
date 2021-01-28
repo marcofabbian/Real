5198e3787d97bb2ee53e9f35b1f04d7db97cbc63
Marco Fabbian <marco.fabbian@gmail.com>
2021-01-28 15:35:52 +0100

    Edit deployment scripts (#22)
    * Added execution riths to the deployment scripts
    
    * Fix typo error
    
    * Fix comand error
    
    * Fix comand error
    
    * Fix comand error
    
    * Fixed bash command to copy artifacts into the dot net core web api directory
    
    * Fixed bash command to copy artifacts into the dot net core web api directory
    
    * Removed sudo comand on copy
    
    * Add utilities stop astart services and copy artifacts

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,24 @@
 #WORKSPACE=/var/lib/jenkins/workspace/RealEstate_Search_Engine
-source_directory=$WORKSPACE/artifacts
-target_directory=/var/www/real.com/dot_net_api
+args=$1
 
-array=$(ls -d $source_directory/*)
+if [ -z $args ]; then
+        sudo echo "Error : empty args."
+        exit 1
+fi
 
-for i in $array
-do
-        cp -R $i $target_directory/
-done
+if [ "$args" = "stopservices" ]; then
+        systemctl stop realservicefile.service
+elif [ "$args" = "deployartefacts" ]; then
+        source_directory=$WORKSPACE/artifacts
+        target_directory=/var/www/real.com/dot_net_api
+
+        array=$(ls -d $source_directory/*)
+
+        for i in $array
+        do
+                sudo cp -R $i $target_directory/
+        done
+
+elif [ "$args" = "startservices" ]; then
+        sudo systemctl restart realservicefile.service
+fi
