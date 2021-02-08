691532afadf03120db23d0874c470fb01ffe777f
marco_fabbian <marco.fabbian@gmail.com>
2021-02-08 15:44:25 +0100

    Added home controller

---
+++ src/Real.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Real.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> log)
+        {
+            this.logger = log;
+        }
+
+        [HttpGet, HttpPost]
+        public IActionResult Get()
+        {
+            logger.LogInformation("Home Controller");
+            return Ok(StatusCodes.Status204NoContent);
+        }
+    }
+}
